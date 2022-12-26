8517e64162999171f60de0bbce0e36ad2fcf6cfe
heeyoung <heeyoung8748@gmail.com>
2022-12-27 01:11:18 +0900

    [Chore] 주석 추가

--- AutoTurnBattle/Assets/Scripts/LivingEntity.cs
+++ AutoTurnBattle/Assets/Scripts/LivingEntity.cs
@@ -1,24 +1,33 @@
 using UnityEngine;
 using UnityEngine.Events;
 
+/*
+ 처음에 Player1, Player2 스크립트를 따로 빼주면서 만들었는데 막상 하다보니
+ 동작이 비슷해 분리할 이유가 없어져서 애매해진 스크립트...
+ */
+
 public class LivingEntity : MonoBehaviour
 {
-    public float Speed { get; protected set; }
-    public int CurrentHealth { get; protected set; }
-    public int CurrentAttackGauge { get; protected set; }
-    public bool IsAlive { get; protected set; }
-    public bool CanAttack { get; set; }
+    public float Speed { get; protected set; } // 게이지 속도
+    public int CurrentHealth { get; protected set; } // 현재 체력
+    public int CurrentAttackGauge { get; protected set; } // 게이지 차오른 정도
+    public bool IsAlive { get; protected set; } // 플레이어 생존 여부
+    public bool IsMyTurn { get; set; } // 플레이어 공격 가능 여부
     
-    protected static int InitialGauge = 100;
+    protected static int InitialGauge = 100; // 게이지 최댓값 초기화용
 
     protected virtual void OnEnable()
     {
         IsAlive = true;
         CurrentHealth = InitialGauge;
         CurrentAttackGauge = 0;
-        CanAttack = true;
+        IsMyTurn = true;
     }
 
+    /// <summary>
+    /// 피격 시 데미지만큼 HP를 감소하고, 사망을 확인하는 함수
+    /// </summary>
+    /// <param name="damage">피격 데미지</param>
     public virtual void OnDamaged(int damage)
     {
         CurrentHealth -= damage;
--- AutoTurnBattle/Assets/Scripts/Player.cs
+++ AutoTurnBattle/Assets/Scripts/Player.cs
@@ -11,16 +11,22 @@ public class Player : LivingEntity
     [SerializeField] private Slider _attackGauge;
     [SerializeField] private TextMeshProUGUI _damagedUI;
 
+    // 속도 범위
     private float _minSpeed = 1f;
     private float _maxSpeed = 10f;
+
+    // 공격 데미지 범위
     private int _minAttackDamage = 1;
     private int _maxAttackDamage = 20;
+
+    // 행동게이지 증가 시 속도를 반영하기 위한 쿨타임
     private float _gaugeCoolTime = 1f;
 
     protected override void OnEnable()
     {
         base.OnEnable();
 
+        // SerializedField로 적용한 UI 초기화
         _hp.maxValue = InitialGauge;
         _hp.value = CurrentHealth;
         _attackGauge.maxValue = InitialGauge;
@@ -35,6 +41,10 @@ public class Player : LivingEntity
         StartCoroutine(IncreaseAttackGauge());
     }
 
+    /// <summary>
+    /// 행동 게이지를 증가시키는 함수
+    /// </summary>
+    /// <returns></returns>
     private IEnumerator IncreaseAttackGauge()
     {
         while (IsAlive)
@@ -42,15 +52,16 @@ public class Player : LivingEntity
             CurrentAttackGauge++;
             _attackGauge.value = CurrentAttackGauge;
 
+            // 게이지가 가득 찼을 시 자신의 턴이라면 공격한다
             if (CurrentAttackGauge >= _attackGauge.maxValue)
             {
-                if (CanAttack)
+                if (IsMyTurn)
                 {
                     Attack();
 
                     CurrentAttackGauge = 0;
                     _attackGauge.value = CurrentAttackGauge;
-                    CanAttack = false;
+                    IsMyTurn = false;
                 }
                 else
                 {
@@ -63,21 +74,35 @@ public class Player : LivingEntity
         }
     }
 
+    /// <summary>
+    /// 랜덤 데미지를 결정하고 적에게 데미지를 입히는 함수
+    /// </summary>
     private void Attack()
     {
         int attackDamage = Random.Range(_minAttackDamage, _maxAttackDamage);
         _enemy.OnDamaged(attackDamage);
-        _enemy.CanAttack = true;
+
+        // 공격이 끝난 후 턴을 넘겨준다
+        _enemy.IsMyTurn = true;
     }
 
+    /// <summary>
+    /// 피격 시 데미지만큼 HP를 감소하고, 사망을 확인하는 함수
+    /// </summary>
+    /// <param name="damage">피격 데미지</param>
     public override void OnDamaged(int damage)
     {
         base.OnDamaged(damage);
+
+        // HP와 피격 데미지를 UI에 적용한다
         _hp.value = CurrentHealth;
         _damagedUI.text = $"{damage}";
         Invoke("RemoveDamageUI", 1f);
     }
 
+    /// <summary>
+    /// 사망 시 행동 게이지 증가를 멈추는 함수
+    /// </summary>
     protected override void Die()
     {
         base.Die();
@@ -85,6 +110,9 @@ public class Player : LivingEntity
         _enemy.StopAllCoroutines();
     }
 
+    /// <summary>
+    /// 화면에 띄운 데미지를 지우는 함수
+    /// </summary>
     private void RemoveDamageUI()
     {
         _damagedUI.text = "";
